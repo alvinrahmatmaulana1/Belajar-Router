53538b6ec824bfeefe3146de2d2829188ebeb2b7
alvinrahmatmaulana1 <akunyang001@gmail.com>
2023-08-01 19:15:42 +0700

    Belajar Router

---
+++ src/assets/Kategori.js
@@ -0,0 +1,26 @@
+
+export const kategori = {
+    "kategori": [
+        {
+            "id": 1,
+            "namakategori": "Elektronik"
+        },
+        {
+            "id": 2,
+            "namakategori": "Fasion"
+        },
+        {
+            "id": 3,
+            "namakategori": "Mainan"
+        },
+        {
+            "id": 4,
+            "namakategori": "Peralatan Rumah Tangga"
+        },
+        {
+            "id": 5,
+            "namakategori": "Kendaraan"
+        },
+    ]
+
+}
---
+++ src/assets/Produk.js
@@ -0,0 +1,97 @@
+
+export const produk = {
+    "produk" : [
+        {
+        "id": 1,
+        "id_kategori" : "1",
+        "nama": "Handphone",
+        "harga" : "Rp 1.000.000,00"
+        },
+        {
+            "id": 2,
+            "id_kategori" : "1",
+            "nama": "Laptop",
+            "harga" : "Rp 5.000.000,00"
+        },
+        {
+            "id": 3,
+            "id_kategori" : "1",
+            "nama": "TV",
+            "harga" : "Rp 3.000.000,00"
+        },
+        {
+            "id": 4,
+            "id_kategori" : "3",
+            "nama": "Tamiya Aero thunder shot",
+            "harga" : "Rp 500.000,00"
+        },
+        {
+            "id": 5,
+            "id_kategori" : "3",
+            "nama": "Yoyo",
+            "harga" : "Rp 100.000,00"
+        },
+        {
+            "id": 6,
+            "id_kategori" : "3",
+            "nama": "Gangsing Beyblade",
+            "harga" : "Rp 250.000,00"
+        },
+        {
+            "id": 7,
+            "id_kategori" : "2",
+            "nama": "Kaos Polos",
+            "harga" : "Rp 30.000,00"
+        },
+        {
+            "id": 8,
+            "id_kategori" : "4",
+            "nama": "Mesin cuci",
+            "harga" : "Rp 7.000.000,00"
+        },
+        {
+            "id": 9,
+            "id_kategori" : "4",
+            "nama": "Kulkas",
+            "harga" : "Rp 10.000.000,00"
+        },
+        {
+            "id": 10,
+            "id_kategori" : "4",
+            "nama": "Lemari",
+            "harga" : "Rp 2.000.000,00"
+        },
+        {
+            "id": 11,
+            "id_kategori" : "5",
+            "nama": "Mobil",
+            "harga" : "Rp 90.000.000,00"
+        },
+        {
+            "id": 12,
+            "id_kategori" : "5",
+            "nama": "Motor",
+            "harga" : "Rp 30.000.000,00"
+        },
+        {
+            "id": 13,
+            "id_kategori" : "5",
+            "nama": "Sepeda",
+            "harga" : "Rp 4.000.000,00"
+        },
+        {
+            "id": 14,
+            "id_kategori" : "2",
+            "nama": "Celana",
+            "harga" : "Rp 300.000,00"
+        },
+        {
+            "id": 14,
+            "id_kategori" : "2",
+            "nama": "Hoodi",
+            "harga" : "Rp 250.000,00"
+        },
+        
+        
+]
+}
